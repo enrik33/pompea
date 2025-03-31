808583792fdff16c61bdb3ac3215d20d05f37e8b
Enrik Cipa <ec5745@rit.edu>
2025-03-31 14:55:40 +0200

    delete backend and discount

--- pompea-backend/routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router, raw } from "express";
-import { createCheckoutSession, verifyPayment } from "../controllers/bookingController.js";
-
-const router = Router();
-
-router.post("/create-checkout-session", createCheckoutSession);
-router.post("/webhook", raw({ type: "application/json" }), verifyPayment);
-
-export default router;
